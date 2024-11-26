5121e274e3c7adb4e51d0fcf88fbad493afeb66a
Dejan Stojanovic <d.stojanovic@hotmail.com>
2024-11-26 13:40:38 +0400

    Add delete method

--- src/Samples.MongoDb.EFCore.Api/Controllers/MoviesController.cs
+++ src/Samples.MongoDb.EFCore.Api/Controllers/MoviesController.cs
@@ -1,5 +1,4 @@
 ﻿using AutoMapper;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Samples.MongoDb.EFCore.Api.Dtos;
@@ -33,8 +32,8 @@ namespace Samples.MongoDb.EFCore.Api.Controllers
         }
 
         [HttpGet("{id}")]
-        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(IEnumerable<MovieViewModel>), Description = "Movie details")]
-        public async Task<ActionResult<IEnumerable<Movie>>> GetMovie(string id)
+        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(IEnumerable<MovieViewModel>), Description = "Retrieve movie details")]
+        public async Task<ActionResult<IEnumerable<MovieViewModel>>> GetMovie(string id)
         {
             var guidId = Guid.Parse(id);
             var movie = await _dbContext.Movies.AsNoTracking().SingleAsync<Movie>(m => m._id == guidId);
@@ -43,17 +42,30 @@ namespace Samples.MongoDb.EFCore.Api.Controllers
         }
 
         [HttpPost]
-        [SwaggerResponse((int)HttpStatusCode.NoContent, Type = typeof(IEnumerable<Movie>), Description = "Add movie")]
-        public async Task<ActionResult<IEnumerable<Movie>>> AddMovie(
+        [SwaggerResponse((int)HttpStatusCode.NoContent, Type = typeof(string), Description = "Add movie")]
+        public async Task<ActionResult<string>> AddMovie(
             [FromBody] MovieAddModel movieAddModel
             )
         {
             var movie = _mapper.Map<Movie>(movieAddModel);
             await _dbContext.Movies.AddAsync(movie);
             await _dbContext.SaveChangesAsync();
-            return CreatedAtAction(actionName: nameof(GetMovie), 
-                                   routeValues: new { id = movie._id.ToString("N") }, 
-                                   value: new { id = movie._id.ToString("N") });
+            return CreatedAtAction(actionName: nameof(GetMovie),
+                                   routeValues: new { id = movie._id.ToString("N") },
+                                   value: movie._id.ToString("N"));
+        }
+
+        [HttpDelete("{id}")]
+        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(void), Description = "Delete movie")]
+        public async Task<ActionResult> DeleteMovie(string id)
+        {
+            var guidId = Guid.Parse(id);
+            var movie = await _dbContext.Movies.SingleAsync<Movie>(m => m._id == guidId);
+
+            _dbContext.Movies.Remove(movie);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
