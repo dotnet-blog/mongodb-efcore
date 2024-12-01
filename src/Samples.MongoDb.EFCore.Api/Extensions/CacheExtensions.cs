018c901d9dd2db1bd18d7136362834ad7210a870
Dejan Stojanovic <d.stojanovic@hotmail.com>
2024-12-01 20:53:58 +0400

    Adds service result caching

---
+++ src/Samples.MongoDb.EFCore.Api/Extensions/CacheExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Samples.MongoDb.EFCore.Api.Extensions
+{
+    public static class CacheExtensions
+    {
+        public static async Task<TInput> GetCached<TInput>(
+            this IDistributedCache cache,
+            String key, 
+            Func<Task<TInput>> func,
+            TimeSpan expiration) where TInput : class
+        {
+            var cached = await cache.GetStringAsync(key);
+            TInput result = null;
+            if (!String.IsNullOrWhiteSpace(cached))
+                result = JsonSerializer.Deserialize<TInput>(cached);
+
+            if (result == null)
+            {
+                var response = await func();
+                result = response;
+                await cache.SetStringAsync(key, JsonSerializer.Serialize(result),
+                    new DistributedCacheEntryOptions().SetAbsoluteExpiration(expiration));
+            }
+            return result;
+        }
+    }
+}
--- src/Samples.MongoDb.EFCore.Api/Services/MovieInfoService.cs
+++ src/Samples.MongoDb.EFCore.Api/Services/MovieInfoService.cs
@@ -1,16 +1,30 @@
 ﻿using Flurl;
+using Microsoft.Extensions.Caching.Distributed;
+using Samples.MongoDb.EFCore.Api.Extensions;
 namespace Samples.MongoDb.EFCore.Api.Services
 {
     public class MovieInfoService : IMovieInfoService
     {
        readonly HttpClient _httpClient;
-        public MovieInfoService(HttpClient httpClient)
+        readonly IDistributedCache _distributedCache;
+        public MovieInfoService(
+            HttpClient httpClient,
+            IDistributedCache distributedCache)
         {
             _httpClient = httpClient;
+            _distributedCache = distributedCache;
         }
         public async Task<MovieInfo?> GetMovieInfo(string imdbId)
         {
-            return await _httpClient.GetFromJsonAsync<MovieInfo?>(_httpClient.BaseAddress.AppendQueryParam("i", imdbId));
+            if(string.IsNullOrWhiteSpace(imdbId))
+                return null;
+
+            var movieInfo = await _distributedCache.GetCached<MovieInfo>(
+                imdbId,
+                () => _httpClient.GetFromJsonAsync<MovieInfo?>(_httpClient.BaseAddress.AppendQueryParam("i", imdbId)),
+                TimeSpan.FromHours(1));
+
+            return movieInfo;
         }
     }
 }
