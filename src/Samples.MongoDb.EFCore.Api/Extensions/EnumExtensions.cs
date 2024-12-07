ade346f7f47f2ae01c221e7423bd80ef7aa4ae41
Dejan Stojanovic <d.stojanovic@hotmail.com>
2024-12-07 22:01:32 +0400

    Adds enum extensions

---
+++ src/Samples.MongoDb.EFCore.Api/Extensions/EnumExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Samples.MongoDb.EFCore.Api.Extensions
+{
+    public static class EnumExtensions
+    {
+        static IDictionary<int, String> GetEnumValueNames(Type type)
+        {
+            var names = type.GetFields(BindingFlags.Public | BindingFlags.Static)
+            .Select(f => f.GetCustomAttribute<DisplayAttribute>()?.Name ?? f.Name);
+
+            var values = Enum.GetValues(type).Cast<int>();
+
+            var dictionary = names.Zip(values, (n, v) => new KeyValuePair<int, string>(v, n))
+            .ToDictionary(kv => kv.Key, kv => kv.Value);
+
+            return dictionary;
+        }
+
+        public static String GetDisplayName<T>(this T value) where T : Enum
+        {
+            var enumValue = Enum.GetValues(typeof(T)).Cast<int>().Single(v => v == System.Convert.ToInt32(value);
+            var dictionary = GetEnumValueNames(value.GetType());
+            return dictionary[enumValue];
+        }
+    }
+}
